2015df8d6f1bde976915330d803b5202bce5b3aa
gusk555 <ghassan.k.kamel@gmail.com>
2023-06-27 23:12:27 +0300

    react router

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import TaskTable from "./routes/TaskTable";
 import TaskInput from "./routes/TaskInput.js";
 import Data from "./TaskData.json";
